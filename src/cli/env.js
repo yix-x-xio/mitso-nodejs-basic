6a7dd550ee1a0c9afd31ac0058be27798d253c9a
yix_x_xio <iivan.krulov@mail.ru>
2025-05-13 01:46:25 +0300

    cli complete

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    const result = Object.entries(process.env)
+        .filter(([key]) => key.startsWith('MITSO_'))
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+    if (result) {
+        console.log(result);
+    }
 };
 
 parseEnv();
